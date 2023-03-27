b0ac91af5aeff71840614f5bad44b41d2f98b237
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-27 20:18:20 +0800

    dom elements

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	element: HTMLFormElement
-
+	titleInputElement: HTMLInputElement
+	descriptionInputElement: HTMLInputElement
+	peopleInputElement: HTMLInputElement
 	constructor() {
 		this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement; 
 		this.hostElement = document.getElementById('app')! as HTMLDivElement; 
@@ -10,9 +12,23 @@ class ProjectInput {
 		const importedNode = document.importNode(this.templateElement.content, true)
 		this.element = importedNode.firstElementChild as HTMLFormElement;
 		this.element.id = 'user-input'
+
+		this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
+		this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
+		this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
+
+
+		this.configure()
 		this.attach()
 	}
 
+	private handleSubmit(event: Event) {
+		event.preventDefault()
+		console.log(this.titleInputElement.value )
+	}
+	private configure() {
+		this.element.addEventListener('submit', this.handleSubmit.bind(this))
+	}
 	private attach() {
 		this.hostElement.insertAdjacentElement('afterbegin', this.element)
 	}
